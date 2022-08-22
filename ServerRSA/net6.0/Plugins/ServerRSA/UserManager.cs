6954131a56686db416832d255786eb9f2c8ee4e9
ncberman <ncberman@asu.edu>
2022-08-22 15:09:52 -0700

    Change to standalone server

---
+++ ServerRSA/net6.0/Plugins/ServerRSA/UserManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DarkRift;
+using DarkRift.Server;
+
+namespace ServerRSA
+{
+    public class UserManager : Plugin
+    {
+        public override bool ThreadSafe => false;
+
+        public override Version Version => new Version(1, 0, 0);
+
+        public UserManager(PluginLoadData pluginLoadData) : base(pluginLoadData)
+        {
+
+        }
+    }
+}
